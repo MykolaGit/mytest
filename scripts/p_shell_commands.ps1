2c01fab374f408b17ce0274a903fef2cb26db105
MykolaGit <50773151+MykolaGit@users.noreply.github.com>
2019-10-03 10:56:35 +0300

    Create p_shell_commands.ps1

---
+++ scripts/p_shell_commands.ps1
@@ -0,0 +1,9 @@
+﻿#Get PWShell version
+Get-Host | Select-Object Version
+
+#Test connection
+Test-Connection www.google.com
+
+Test-NetConnection www.google.com
+Test-NetConnection www.google.com -InformationLevel "Detailed"
+Test-NetConnection www.google.com -Port 443
